db3832afc98d7e6a8b15653cb24eac2bac5698c9
Jake Hartz <jake33@rocketmail.com>
2012-07-15 15:43:12 -0400

    Added default port

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Haunted House Logistics Server - config file
 
-exports.PORT = process.env.PORT;
+exports.PORT = process.env.PORT || 80;
 exports.VIDEO_DIR = "videos";
 exports.SOUND_DIR = "sounds";
 exports.RESOURCES_DIR = "resources";
