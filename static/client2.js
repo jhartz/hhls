f8175fb73257fbf7b7ae00767cd2131912a370a5
Jake Hartz <jake33@rocketmail.com>
2012-07-20 00:04:26 -0400

    Bugfix for when close all button is hidden

--- static/client2.js
+++ static/client2.js
@@ -1,19 +1,23 @@
 window.onload = function () {
     if (document.getElementById && document.getElementsByTagName && document.addEventListener) {
-        document.getElementById("status_closeall").addEventListener("click", function (event) {
-            var frames = document.getElementsByTagName("iframe");
-            for (var i = 0; i < frames.length; i++) {
-                if (frames[i] && frames[i].contentWindow && typeof frames[i].contentWindow.closeconn == "function") {
-                    frames[i].contentWindow.closeconn();
+        if (document.getElementById("status_closeall")) {
+            document.getElementById("status_closeall").addEventListener("click", function (event) {
+                var frames = document.getElementsByTagName("iframe");
+                for (var i = 0; i < frames.length; i++) {
+                    if (frames[i] && frames[i].contentWindow && typeof frames[i].contentWindow.closeconn == "function") {
+                        frames[i].contentWindow.closeconn();
+                    }
                 }
-            }
-        }, false);
+            }, false);
+        }
         
-        document.getElementById("intercom_checkbox").addEventListener("change", function (event) {
-            if (document.getElementById("intercom_checkbox").checked) {
-                alert("TODO: Intercom!");
-            }
-        }, false);
+        if (document.getElementById("intercom_checkbox")) {
+            document.getElementById("intercom_checkbox").addEventListener("change", function (event) {
+                if (document.getElementById("intercom_checkbox").checked) {
+                    alert("TODO: Intercom!");
+                }
+            }, false);
+        }
         
         document.getElementById("maincontent").style.display = "block";
     } else {
