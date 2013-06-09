1a4d360df8afe09046f9c2931a9bbf141b62cff1
Jake Hartz <jhartz@outlook.com>
2013-06-09 00:25:00 -0400

    Working on adding sequences

---
+++ static/control.sequences.js
@@ -0,0 +1,37 @@
+var sequences = {
+    init: function () {
+        $("#ctrl_sequences > footer button").click(function () {
+            if ($(this).hasClass("active")) {
+                sequences.toggle("main");
+            } else {
+                sequences.toggle($(this).attr("data-btn"));
+            }
+        });
+        
+        // Load settings
+        this.update_sequences();
+    },
+    
+    toggle: function (newbie, oncomplete) {
+        toggle_buttonbox("sequences", newbie, oncomplete);
+    },
+    
+    update_sequences: function () {
+        // TODO
+    }
+};
+
+init.push(function () {
+    sequences.init();
+});
+
+onConnection.push(function () {
+    sequences.toggle("controls");
+});
+onNoConnection.push(function () {
+    sequences.toggle("waiting");
+});
+
+settings_onupdate.sequences.push(function () {
+    sequences.update_sequences();
+});
